6cedb990de044808f3782aca8710f4b42dd2882d
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2016-04-15 22:20:02 +0530

    Login basic done. Testing required

--- js/main.js
+++ js/main.js
@@ -3,14 +3,44 @@ var app = angular.module('researchHubApp', ['ngRoute']);
 /**
  * Route Configuration
  */
+rootPath = 'http://localhost/laravel/public/';
+
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
+        // Index
+        .when("/", {templateUrl: "partials/home.html", controller: "indexController"})
         // Home
-        .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+        .when("/home", {templateUrl: "partials/wall.html", controller: "wallController"});
         // Pages
-        .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+        //.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
-app.controller('PageCtrl', function ($scope, $location, $http) {
-    console.log("Page Controller reporting for duty.");
+app.controller('indexController', function ($scope, $location, $http) {
+    $http.jsonp(rootPath + 'user' + '?callback=JSON_CALLBACK').then(successCallback, errorCallback);
+    function successCallback(response) {
+        console.log(response);
+        if(response.data.status == 'logged_in') {
+            $location.path('/home');
+        }        
+    }
+    function errorCallback() {
+        alert("There has been an error. Please refresh the page or try again after a few minutes");
+    }
+});
+app.controller('wallController', function ($scope, $location, $http) {
+    $http.jsonp(rootPath + 'user' + '?callback=JSON_CALLBACK').then(successCallback, errorCallback);
+    function successCallback(response) {
+        console.log(response);
+        if(response.data.status == 'not_logged_in') {
+            $location.path('/');
+        }
+        else {
+            $scope.user = response.data.user;
+            console.log($scope.user);
+        }
+        
+    }
+    function errorCallback() {
+        alert("There has been an error. Please refresh the page or try again after a few minutes");
+    }
 });
